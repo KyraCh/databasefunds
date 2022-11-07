06b2e50f3d22ce4a26127f3ac40a73c7f93a9bb5
mariausinclair <115650009+mariausinclair@users.noreply.github.com>
2022-11-07 14:55:26 +0300

    Update browse.php

--- browse.php
+++ browse.php
@@ -1,5 +1,6 @@
 <?php include_once("header.php")?>
 <?php require("utilities.php")?>
+<?php include("connection.php")?>
 
 <div class="container">
 
@@ -147,27 +148,23 @@
      retrieved from the query -->
 
 <?php
-  // Demonstration of what listings will look like using dummy data.
-  $item_id = "87021";
-  $title = "Dummy title";
-  $description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum eget rutrum ipsum. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Phasellus feugiat, ipsum vel egestas elementum, sem mi vestibulum eros, et facilisis dui nisi eget metus. In non elit felis. Ut lacus sem, pulvinar ultricies pretium sed, viverra ac sapien. Vivamus condimentum aliquam rutrum. Phasellus iaculis faucibus pellentesque. Sed sem urna, maximus vitae cursus id, malesuada nec lectus. Vestibulum scelerisque vulputate elit ut laoreet. Praesent vitae orci sed metus varius posuere sagittis non mi.";
-  $current_price = 30;
-  $num_bids = 1;
-  $end_date = new DateTime('2020-09-16T11:00:00');
-  
-  // This uses a function defined in utilities.php
-  print_listing_li($item_id, $title, $description, $current_price, $num_bids, $end_date);
-  
-  $item_id = "516";
-  $title = "Different title";
-  $description = "Very short description.";
-  $current_price = 13.50;
-  $num_bids = 3;
-  $end_date = new DateTime('2020-11-02T00:00:00');
-  
-  print_listing_li($item_id, $title, $description, $current_price, $num_bids, $end_date);
-?>
 
+            $sql = "SELECT * FROM  auction1;";
+            $result = mysqli_query($con, $sql);
+            $check = mysqli_num_rows($result);
+
+            if ($check > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    $item_id = $row['auctionId'];
+                    $title = $row["title"];
+                    $description = $row["details"];
+                    $current_price = $row["reservePrice"];
+                    $num_bids = 1;
+                    $end_date = $row["endDate"];
+                    print_listing_li($item_id, $title, $description, $current_price, $num_bids, $end_date);
+                }
+            };
+            ?>
 </ul>
 
 <!-- Pagination for results listings -->
