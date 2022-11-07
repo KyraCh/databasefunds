39b8cf5b4b9759bcfc782036d6eeb50d356595a8
KyraCh <92988384+KyraCh@users.noreply.github.com>
2022-11-07 13:42:43 +0000

    Create connection copy.php

---
+++ connection copy.php
@@ -0,0 +1,13 @@
+<?php
+
+$dbhost = "localhost";
+$dbuser = "root";
+$dbpass = "";
+$dbname = "auction_system";
+
+if (!$con = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname)) {
+
+    die("failed to connect!");
+}
+
+
