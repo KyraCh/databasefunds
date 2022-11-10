9591de644205924110827910d662781ee9e2dc85
mariausinclair <115650009+mariausinclair@users.noreply.github.com>
2022-11-10 22:41:37 +0300

    recomendations working

--- recommendations.php
+++ recommendations.php
@@ -1,22 +1,36 @@
 <?php include_once("header.php")?>
 <?php require("utilities.php")?>
+<?php include("connection.php")?>
 
 <div class="container">
 
-<h2 class="my-3">Recommendations for you</h2>
-
+<h2 class="my-3">Recommendations for you:</h2>
 <?php
-  // This page is for showing a buyer recommended items based on their bid 
-  // history. It will be pretty similar to browse.php, except there is no 
-  // search bar. This can be started after browse.php is working with a database.
-  // Feel free to extract out useful functions from browse.php and put them in
-  // the shared "utilities.php" where they can be shared by multiple files.
-  
-  
-  // TODO: Check user's credentials (cookie/session).
-  
-  // TODO: Perform a query to pull up auctions they might be interested in.
-  
-  // TODO: Loop through results and print them out as list items.
-  
-?>
+$email = $_SESSION['email'];
+$now = new DateTime();
+
+    $sql = "SELECT * FROM auction1 WHERE auctionId IN 
+                             (SELECT DISTINCT auctionId from bid where email in 
+                            (SELECT email from bid where auctionId in 
+                            (SELECT auctionId FROM `bid` WHERE email='$email')) and auctionId not in 
+                            (SELECT auctionId FROM `bid` WHERE email='$email'));";
+    $result = mysqli_query($con, $sql);
+    $check = mysqli_num_rows($result);
+
+    if ($check > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+    $item_id = $row['auctionId'];
+    $title = $row["title"];
+    $description = $row["details"];
+    $num_bids = $row["num_bids"];
+    $db_email = $row["email"];
+    if ($num_bids==0){
+        $current_price = $row["startingPrice"];
+    }
+    else {
+            $current_price = $row["reservePrice"];
+        }
+    $end_date = new DateTime($row["endDate"]);
+    if ($now < $end_date) {
+        print_listing_li($item_id, $title, $description, $current_price, $num_bids, $end_date);}}}
+
