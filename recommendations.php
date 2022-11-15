7f8041b5a3ba9ce332ae8be045b18d838bb33910
mariausinclair <115650009+mariausinclair@users.noreply.github.com>
2022-11-15 11:42:14 +0000

    Update recommendations.php

--- recommendations.php
+++ recommendations.php
@@ -1,22 +1,52 @@
 <?php include_once("header.php")?>
 <?php require("utilities.php")?>
+<?php include("connection.php")?>
 
-<div class="container">
+    <div class="container">
 
-<h2 class="my-3">Recommendations for you</h2>
-
+                <h2 class="my-3">Recommendations for you</h2>
 <?php
-  // This page is for showing a buyer recommended items based on their bid 
-  // history. It will be pretty similar to browse.php, except there is no 
-  // search bar. This can be started after browse.php is working with a database.
-  // Feel free to extract out useful functions from browse.php and put them in
-  // the shared "utilities.php" where they can be shared by multiple files.
-  
-  
-  // TODO: Check user's credentials (cookie/session).
-  
-  // TODO: Perform a query to pull up auctions they might be interested in.
-  
-  // TODO: Loop through results and print them out as list items.
-  
-?>
+$email = $_SESSION['email'];
+$now = new DateTime();
+
+$sql = "SELECT DISTINCT category FROM auction1;";
+$result = mysqli_query($con, $sql);
+$check = mysqli_num_rows($result);
+if ($check>0) {
+    while ($row = mysqli_fetch_assoc($result)){
+        $category = $row['category'];
+        $sql2 = "SELECT * FROM auction1 WHERE auctionId IN( 
+        SELECT auctionId FROM bid WHERE email IN( 
+        SELECT email FROM bid WHERE auctionId IN ( 
+        SELECT auctionId FROM bid WHERE email = 'bobmu@outlook.com'))) 
+        AND auctionId NOT IN ( 
+        SELECT auctionId FROM bid WHERE email = 'bobmu@outlook.com') 
+        AND endDate>now();";
+        $result2 = mysqli_query($con, $sql2);
+        $check2 = mysqli_num_rows($result2);
+        if ($check2>0) {
+            while ($row = mysqli_fetch_assoc($result2)) {
+                $item_id = $row['auctionId'];
+                $title = $row["title"];
+                $description = $row["details"];
+                $num_bids = $row["num_bids"];
+                $e_mail = $row['email'];
+                $category_item = $row['category'];
+                if ($num_bids==0){
+                    $current_price = $row["startingPrice"];
+                }
+                else {
+                    $current_price = $row["reservePrice"];
+                }
+                $end_date = new DateTime($row["endDate"]);
+                if ($category == $category_item) {
+                    ?> <h4 class="my-3">More in <?php echo $category?>:</h4><?php
+                    print_listing_li($item_id, $title, $description, $current_price, $num_bids, $end_date);
+
+                }
+
+
+
+    }
+
+}}}
