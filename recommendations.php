31498ecf3299b4f4e9268025514b7dd505d89b57
mariausinclair <115650009+mariausinclair@users.noreply.github.com>
2022-11-19 11:43:01 +0000

    update in recomendations

--- recommendations.php
+++ recommendations.php
@@ -9,6 +9,36 @@
 $email = $_SESSION['email'];
 $now = new DateTime();
 
+//display items which have highest number of bids from last week
+$sql = "SELECT * from auction1 INNER JOIN
+(SELECT auctionId, COUNT(*) AS c FROM bid WHERE date BETWEEN date_sub(now(),INTERVAL 1 WEEK) AND now() GROUP BY auctionId) AS c_table
+WHERE auction1.auctionId = c_table.auctionId ORDER BY c DESC LIMIT 3;";
+$result = mysqli_query($con, $sql);
+$check = mysqli_num_rows($result);
+//only print if such items exist
+if ($check >0) {
+    ?>
+    <h4 class="my-3">Trending this week</h4>
+    <?php
+    while ($row = mysqli_fetch_assoc($result)) {
+                $item_id = $row['auctionId'];
+                $title = $row["title"];
+                $description = $row["details"];
+                $num_bids = $row["num_bids"];
+                $e_mail = $row['email'];
+                if ($num_bids==0){
+                    $current_price = $row["startingPrice"];
+                }
+                else {
+                    $current_price = $row["reservePrice"];
+                }
+                $end_date = new DateTime($row["endDate"]);
+                if ($end_date > $now) {
+                print_listing_li($item_id, $title, $description, $current_price, $num_bids, $end_date);
+                }
+    }
+}
+
 $sql = "SELECT DISTINCT category FROM auction1;";
 $result = mysqli_query($con, $sql);
 $check = mysqli_num_rows($result);
@@ -18,9 +48,9 @@ if ($check>0) {
         $sql2 = "SELECT * FROM auction1 WHERE auctionId IN( 
         SELECT auctionId FROM bid WHERE email IN( 
         SELECT email FROM bid WHERE auctionId IN ( 
-        SELECT auctionId FROM bid WHERE email = 'bobmu@outlook.com'))) 
+        SELECT auctionId FROM bid WHERE email = '$email'))) 
         AND auctionId NOT IN ( 
-        SELECT auctionId FROM bid WHERE email = 'bobmu@outlook.com') 
+        SELECT auctionId FROM bid WHERE email = '$email') 
         AND endDate>now();";
         $result2 = mysqli_query($con, $sql2);
         $check2 = mysqli_num_rows($result2);
