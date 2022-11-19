5da2c16ac2534e8ada7eeb80584c448e034a75e2
mariausinclair <115650009+mariausinclair@users.noreply.github.com>
2022-11-19 12:46:28 +0000

    update recs

--- recommendations.php
+++ recommendations.php
@@ -37,7 +37,7 @@ if ($check >0) {
     }
 }
 
-//recommendations based on items that the user previously bid on
+//recommendations based on collaborative filtering for each item user bid on (3 most recent)
 $sql = "SELECT DISTINCT title, auction1.auctionId FROM auction1 
 INNER JOIN bid 
 WHERE auction1.auctionId = bid.auctionId
@@ -87,45 +87,46 @@ LIMIT 3;";
 }
 }
 
-
-
-
-
-
-$sql = "SELECT DISTINCT category FROM auction1;";
-$result = mysqli_query($con, $sql);
-$check = mysqli_num_rows($result);
-if ($check>0) {
-    while ($row = mysqli_fetch_assoc($result)){
-        $category = $row['category'];
-        $sql2 = "SELECT * FROM auction1 WHERE auctionId IN( 
-        SELECT auctionId FROM bid WHERE email IN( 
-        SELECT email FROM bid WHERE auctionId IN ( 
-        SELECT auctionId FROM bid WHERE email = '$email'))) 
-        AND auctionId NOT IN ( 
-        SELECT auctionId FROM bid WHERE email = '$email') 
-        AND endDate>now();";
-        $result2 = mysqli_query($con, $sql2);
-        $check2 = mysqli_num_rows($result2);
-        if ($check2>0) {
-            while ($row = mysqli_fetch_assoc($result2)) {
-                $item_id = $row['auctionId'];
-                $title = $row["title"];
-                $description = $row["details"];
-                $num_bids = $row["num_bids"];
-                $e_mail = $row['email'];
-                $category_item = $row['category'];
-                if ($num_bids==0){
-                    $current_price = $row["startingPrice"];
+//recommendations based on categories of items that user has previously bid on
+$sql_cat = "SELECT DISTINCT category FROM auction1
+INNER JOIN
+(SELECT auctionId, date FROM bid WHERE email = 'bobmu@outlook.com') AS bids
+ON auction1.auctionId = bids.auctionId
+ORDER BY bids.date
+LIMIT 3;";
+$result_cat = mysqli_query($con, $sql_cat);
+$check_cat = mysqli_num_rows($result_cat);
+if ($check_cat>0){
+    while ($row = mysqli_fetch_assoc($result_cat)){
+            $category = $row['category'];
+            $sql_rec_cat = "SELECT DISTINCT title, details, reservePrice, endDate, num_bids, b.auctionId, category FROM auction1 
+INNER JOIN bid as b
+ON b.auctionId = auction1.auctionId
+WHERE category = '$category'
+AND b.email != '$email';";
+            $result_rec_cat = mysqli_query($con, $sql_rec_cat);
+            $check_rec_cat = mysqli_num_rows($result_rec_cat);
+            if ($check_rec_cat>0){
+                ?>
+                <h4 class="my-3">More in <?php echo $category?></h4>
+                <?php
+                while ($row = mysqli_fetch_assoc($result_rec_cat)){
+                    $item_id = $row['auctionId'];
+                    $title = $row["title"];
+                    $description = $row["details"];
+                    $num_bids = $row["num_bids"];
+                    if ($num_bids==0){
+                        $current_price = $row["startingPrice"];
+                    }
+                    else {
+                        $current_price = $row["reservePrice"];
+                    }
+                    $end_date = new DateTime($row["endDate"]);
+                    if ($end_date > $now){
+                        print_listing_li($item_id, $title, $description, $current_price, $num_bids, $end_date);
+                    }
                 }
-                else {
-                    $current_price = $row["reservePrice"];
-                }
-                $end_date = new DateTime($row["endDate"]);
-                if ($category == $category_item) {
-                    ?> <h4 class="my-3">More in <?php echo $category?>:</h4><?php
-                    print_listing_li($item_id, $title, $description, $current_price, $num_bids, $end_date);
-
-                }
+            }
     }
-}}}
+}
+
