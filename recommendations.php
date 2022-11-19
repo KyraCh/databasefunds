7c4bc15282bcf21704f7c69539f12d019efc281e
mariausinclair <115650009+mariausinclair@users.noreply.github.com>
2022-11-19 12:21:42 +0000

    recs

--- recommendations.php
+++ recommendations.php
@@ -3,8 +3,6 @@
 <?php include("connection.php")?>
 
     <div class="container">
-
-                <h2 class="my-3">Recommendations for you</h2>
 <?php
 $email = $_SESSION['email'];
 $now = new DateTime();
@@ -38,6 +36,61 @@ if ($check >0) {
                 }
     }
 }
+
+//recommendations based on items that the user previously bid on
+$sql = "SELECT DISTINCT title, auction1.auctionId FROM auction1 
+INNER JOIN bid 
+WHERE auction1.auctionId = bid.auctionId
+AND bid.email = '$email' 
+ORDER BY date DESC
+LIMIT 3;";
+$result = mysqli_query($con, $sql);
+$check = mysqli_num_rows($result);
+if ($check>0){
+    while ($row = mysqli_fetch_assoc($result)){
+        $item_id_bid = $row['auctionId'];
+        $title_bid = $row['title'];
+        $sql_rec = "SELECT * FROM auction1 
+WHERE auctionId IN
+(SELECT DISTINCT auctionId FROM bid 
+WHERE email IN
+(SELECT email FROM bid WHERE auctionId = $item_id_bid)
+AND email != '$email')
+AND auctionId NOT IN
+(SELECT auctionId FROM bid
+WHERE email = '$email')
+LIMIT 3;";
+        $result_rec = mysqli_query($con, $sql_rec);
+        $check_rec = mysqli_num_rows($result_rec);
+        if ($check_rec>0){
+            ?>
+        <h4 class="my-3">Recommended for you based on <?php echo $title_bid?></h4>
+        <?php
+        while ($row = mysqli_fetch_assoc($result_rec)){
+            $item_id = $row['auctionId'];
+            $title = $row["title"];
+            $description = $row["details"];
+            $num_bids = $row["num_bids"];
+            $e_mail = $row['email'];
+            if ($num_bids==0){
+                $current_price = $row["startingPrice"];
+            }
+            else {
+                $current_price = $row["reservePrice"];
+            }
+            $end_date = new DateTime($row["endDate"]);
+            if ($end_date > $now){
+                print_listing_li($item_id, $title, $description, $current_price, $num_bids, $end_date);
+            }
+        }
+    }
+}
+}
+
+
+
+
+
 
 $sql = "SELECT DISTINCT category FROM auction1;";
 $result = mysqli_query($con, $sql);
